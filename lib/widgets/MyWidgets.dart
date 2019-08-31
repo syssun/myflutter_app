e8ad21a7fe756ade3fb99c6404aab5e880734da2
syssun <2506715686@qq.com>
2019-09-01 00:33:22 +0800

    Initial commit

---
+++ lib/widgets/MyWidgets.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+class MyWidgets {
+
+  static Widget buildAppBar(BuildContext context,String title){
+    return new AppBar(title:new Text(title),centerTitle: true,);
+  }
+  static Image imageNet(String src){
+    return new Image.network(src,fit: BoxFit.fill,);
+  }
+  static Image imageAssetWithSize(String src,{double size=50.0}){
+    return new Image.asset(src,width: size,height: size,fit: BoxFit.fill,);
+  }
+  static Padding getText1(title){
+    TextStyle textStyle = new TextStyle(fontWeight: FontWeight.normal, fontSize: 14.0);
+    return new Padding(
+        padding: const EdgeInsets.only(top: 10),
+        child: new Text(title,style: textStyle)
+    );
+  }
+}
